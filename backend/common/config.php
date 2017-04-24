90552527d0f7e9d80978b6044dc0523ed44ddc8e
mbolli <michael@bolli.us>
2017-04-24 10:50:59 +0200

    config checks for writing permissions, loads class directly from settings file.

--- backend/common/config.php
+++ backend/common/config.php
@@ -13,18 +13,24 @@ abstract class Config {
     public static function initialize() {
         if (self::$initialized === true) return;
 
-        $settings_file = getcwd() . DIRECTORY_SEPARATOR . "settings" . DIRECTORY_SEPARATOR . "settings.php";
-        if (!file_exists($settings_file)) throw new \Exception('No settings.php found.');
+        $settings_file = getcwd() . DIRECTORY_SEPARATOR . 'settings' . DIRECTORY_SEPARATOR . 'settings.php';
+        if (!file_exists($settings_file)) throw new \Exception('No settings.php found. Did you rename the distributed settings correctly?');
         include($settings_file);
         self::$cfg = $nfsen_config;
-        self::$path = __DIR__;
+        self::$path = getcwd();
         self::$initialized = true;
 
         // find data source
-        if(array_key_exists('host', self::$cfg['db']['akumuli'])) {
-            self::$db = new \datasources\Akumuli();
+        $db_class = 'datasources\\' . self::$cfg['general']['db'];
+        if (class_exists($db_class)) {
+            self::$db = new $db_class();
         } else {
-            self::$db = new \datasources\RRD();
+            throw new \Exception('Failed loading class ' . self::$cfg['general']['db'] . '. The class doesn\'t exist.');
+        }
+
+        // check if folders have correct access rights
+        if (!is_writable(self::$db->get_data_path())) {
+            throw new \Exception('Cannot write to ' . self::$db->get_data_path() . '!', LOG_CRIT);
         }
     }
 

71d6d936a7fcf1a63e6e57ebc069ef9fa16266d0
mbolli <michael@bolli.us>
2017-05-02 21:50:15 +0300

    enable zooming the graph and dynamically get more detailed data

--- frontend/js/nfsen-ng.js
+++ frontend/js/nfsen-ng.js
@@ -87,17 +87,31 @@ $(document).ready(function() {
     // GRAPH VIEW
     $(document).on('change', '#curves input', function(e) {
         var $checkbox = $(e.target);
-        graph.setVisibility($checkbox.parent().index(), $($checkbox).is(':checked'));
+        dygraph.setVisibility($checkbox.parent().index(), $($checkbox).is(':checked'));
     });
 
     // initialize application
     function init() {
 
+        // check if we have a config
+        // todo probably a red half-transparent overlay over the whole page?
+        if (typeof config !== 'object') console.log('Could not read config!');
+        updateSources(Object.keys(config['sources']));
+
+        init_rangeslider();
+
+        updateGraph();
+    }
+
+    /**
+     * initialize the range slider
+     */
+    function init_rangeslider() {
         // initialize date range slider
         $('#date_range').ionRangeSlider({
             type: 'double',
             grid: true,
-            min: 1482828600000,
+            min: 1482828600000, // todo set dates with some logic
             max: 1490604300000,
             force_edges: true,
             drag_interval: true,
@@ -110,13 +124,79 @@ $(document).ready(function() {
             }
         });
         date_range = $('#date_range').data('ionRangeSlider');
+    }
 
-        // check if we have a config
-        // todo probably a red half-transparent overlay over the whole page?
-        if (typeof config !== 'object') console.log('Could not read config!');
+    /**
+     * initialize two dygraph mods
+     * they are needed to dynamically load more detailed data as the user zooms in or pans around
+     * heavily influenced by https://github.com/kaliatech/dygraphs-dynamiczooming-example
+     */
+    function init_dygraph_mods() {
+        dygraph_rangeselector_active = false;
+        var $rangeEl = $('#flowDiv').find('.dygraph-rangesel-fgcanvas, .dygraph-rangesel-zoomhandle');
 
-        updateSources(Object.keys(config["sources"]));
-        updateGraph();
+        // uninstall existing handler if already installed
+        $rangeEl.off('mousedown touchstart');
+
+        // install new mouse down handler
+        $rangeEl.on('mousedown touchstart', function () {
+
+            // track that mouse is down on range selector
+            dygraph_rangeselector_active = true;
+
+            // setup mouse up handler to initiate new data load
+            $(window).off('mouseup touchend'); //cancel any existing
+            $(window).on('mouseup touchend', function () {
+                $(window).off('mouseup touchend');
+
+                // mouse no longer down on range selector
+                dygraph_rangeselector_active = false;
+
+                // get the new detail window extents
+                dygraph_daterange = dygraph.xAxisRange();
+                updateGraph();
+            });
+        });
+
+        // save original endPan function
+        var origEndPan = Dygraph.defaultInteractionModel.endPan;
+
+        // replace built-in handling with our own function
+        Dygraph.defaultInteractionModel.endPan = function (event, g, context) {
+
+            // call the original to let it do it's magic
+            origEndPan(event, g, context);
+
+            // extract new start/end from the x-axis
+            dygraph_daterange = g.xAxisRange();
+            updateGraph();
+        };
+        Dygraph.endPan = Dygraph.defaultInteractionModel.endPan; // see dygraph-interaction-model.js
+    }
+
+    /**
+     * zoom callback for dygraph
+     * updates the graph when the rangeselector is not active
+     * @param minDate
+     * @param maxDate
+     */
+    function dygraph_zoom(minDate, maxDate) {
+        dygraph_daterange = [new Date(minDate), new Date(maxDate)];
+
+        //When zoom reset via double-click, there is no mouse-up event in chrome (maybe a bug?),
+        //so we initiate data load directly
+        if (dygraph.isZoomed('x') === false) {
+            $(window).off('mouseup touchend'); //Cancel current event handler if any
+            updateGraph();
+            return;
+        }
+
+        //The zoom callback is called when zooming via mouse drag on graph area, as well as when
+        //dragging the range selector bars. We only want to initiate dataload when mouse-drag zooming. The mouse
+        //up handler takes care of loading data when dragging range selector bars.
+        if (!dygraph_rangeselector_active)
+            updateGraph();
+
     }
 
     /**
@@ -129,14 +209,14 @@ $(document).ready(function() {
             protos = $('#graphsFilterProtocolDiv').find('input:checked').map(function() { return $(this).val(); }).get(),
             title = type + ' for ';
 
-        // check if options valid to request new graph
+        // check if options valid to request new dygraph
         if (protos.length === 0 || sources.length === 0) return;
         if (protos.length > 1 && sources.length > 1) return; // todo annotate wrong input?
 
         // set options
         api_graph_options = {
-            datestart: getFirstDate(sources)/1000,
-            dateend: getLastDate(sources)/1000,
+            datestart: dygraph_daterange ? parseInt(dygraph_daterange[0]/1000) : getFirstDate(sources)/1000,
+            dateend: dygraph_daterange ? parseInt(dygraph_daterange[1]/1000) : getLastDate(sources)/1000,
             type: type,
             protocols: protos,
             sources: sources,
@@ -155,41 +235,56 @@ $(document).ready(function() {
 
                 // iterate over labels
                 $('#curves').empty();
-                $.each(data.legend, function(id, legend) {
+                $.each(data.legend, function (id, legend) {
                     labels.push(legend);
 
                     $('#curves').append('<label><input type="checkbox" checked> ' + legend + '</label>');
                 });
 
                 // iterate over values
-                $.each(data.data, function(datetime) {
-                    var position = [ new Date(datetime*1000) ];
+                $.each(data.data, function (datetime) {
+                    var position = [new Date(datetime * 1000)];
 
-                    $.each(this, function(y, val) {
+                    $.each(this, function (y, val) {
                         position.push(val);
                     });
 
                     dygraph_data.push(position);
                 });
 
-                graph = new Dygraph(
-                    $('#flowDiv')[0],
-                    dygraph_data, {
-                        title : title,
+                if (typeof dygraph === 'undefined') {
+                    // initial dygraph config:
+                    dygraph_config = {
+                        title: title,
                         labels: labels,
-                        ylabel : type.toUpperCase(),
-                        xlabel : 'TIME',
-                        labelsKMB : true,
-                        labelsDiv : $('#legend')[0],
-                        labelsSeparateLines : true,
-                        legend : 'always',
+                        ylabel: type.toUpperCase(),
+                        xlabel: 'TIME',
+                        labelsKMB: true,
+                        labelsDiv: $('#legend')[0],
+                        labelsSeparateLines: true,
+                        legend: 'always',
                         showRangeSelector: true,
-                        dateWindow: [dygraph_data[0][0], dygraph_data[dygraph_data.length-1][0]],
+                        dateWindow: [dygraph_data[0][0], dygraph_data[dygraph_data.length - 1][0]],
+                        zoomCallback: dygraph_zoom
                         // todo add current values of logscale, stackedGraph and fillGraph // galld2 comment, not needed, all false by default on load
-                    }
-                );
+                    };
+                    dygraph = new Dygraph($('#flowDiv')[0], dygraph_data, dygraph_config);
+                } else {
+                    // update dygraph config
+                    dygraph_config = {
+                        // series: series,
+                        // axes: axes,
+                        labels: labels,
+                        file: dygraph_data,
+                        dateWindow: [dygraph_data[0][0], dygraph_data[dygraph_data.length - 1][0]],
+                    };
+                    dygraph.updateOptions(dygraph_config);
+
+                }
+                init_dygraph_mods();
+
             } else {
-                console.log('There was probably a problem with getting graph data.');
+                console.log('There was probably a problem with getting dygraph data.');
                 // todo consequences?
             }
         });
